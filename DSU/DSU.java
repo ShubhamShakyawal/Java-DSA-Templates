342c502c95a2960457a1d1bc25e82801e06c8fd4
ShubhamShakyawal <shakyawals@gmail.com>
2026-03-30 23:47:42 +0530

    dsu file added

---
+++ DSU/DSU.java
@@ -0,0 +1,26 @@
+class DSU {
+    int[] parent;
+    int[] size;
+
+    public int find(int x ) {
+        if(parent[x] == x) return x;
+	// path compression
+        return parent[x] = find(parent[x]);
+    }
+
+    public void union(int i,int j) {
+        int rootI = find(i);
+        int rootJ = find(j);
+
+	// compare only if roots are different
+        if(rootI != rootJ) {
+            if(size[rootI] >= size[rootJ]) {
+                parent[rootJ] = parent[rootI];
+                size[rootI] += size[rootJ];
+            } else {
+                parent[rootI] = rootJ;
+                size[rootJ] += size[rootI];
+            }
+        }
+    }
+}
